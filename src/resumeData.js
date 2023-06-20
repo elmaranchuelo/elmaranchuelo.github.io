08f75620039738bfec938d5b395b180f994c18e0
elmaranchuelo <elmaranchuelo@gmail.com>
2023-06-21 00:50:20 +0800

    added react files

---
+++ src/resumeData.js
@@ -0,0 +1,115 @@
+let resumeData = {
+  "imagebaseurl":"https://github.com/elmaranchuelo",
+    "name": "ELMAR JOHN ANCHUELO",
+    "role": "Full Stack Web Developer",
+  "linkedinId":"ejanchuelo",
+  "skypeid": "ejanchuelo",
+  "roleDescription": " A passionate Web Developer with a proficiency in doing both frontend and backend. Let's start scrolling and learn more about me.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ejanchuelo/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/elmaranchuelo",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+  "aboutme": "I've completed my Bachelor’s in Information Technology, but everything I know of and specialize in, has been self-taught. I’ve always enjoyed learning new stuff for self growth.  With several years of experience in the field, I have developed a deep understanding of web technologies and an ability to bring ideas to life through coding",
+    "address":"General Trias Cavite, Philippines",
+  "website":"elmaranchuelo@gmail.com 09267084139",
+    "education":[
+      {
+        "UniversityName":"Lyceum of the Philippines - Cavite",
+        "specialization":"Bachelor of Science in Information Technology",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2017",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Palvision",
+        "specialization":"Senior Web Developer",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2023",
+        "Achievements":"Work for several projects doing both frontend and backend."
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"ReactJS"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"Laravel"
+      },
+      {
+        "skillname":"CSHARP"
+      },
+    ],
+    "portfolio":[
+      {
+        "id": "0",
+        "name":"IPTV",
+        "description":"Web applications within Hospitality TV",
+        "imgurl":"images/portfolio/IPTV.gif"
+      },
+      {
+        "id": "1",
+        "name":"CMS",
+        "description":"Content Management System",
+        "imgurl":"images/portfolio/CMS.jpg"
+      },{
+        "id": "3",
+        "name": "Website",
+        "description": "Wordpress website ",
+        "imgurl": "images/portfolio/websitee.jpg"
+      }, {
+        "id": "2",
+        "name": "Chatbot",
+        "description": "Chatbot using Dialogflow",
+        "imgurl": "images/portfolio/chatbot.gif"
+      }, 
+      {
+        "id": "4",
+        "name": "Mobile Web App",
+        "description": "Mobile Web App ",
+        "imgurl": "images/portfolio/mobileapp.gif"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
