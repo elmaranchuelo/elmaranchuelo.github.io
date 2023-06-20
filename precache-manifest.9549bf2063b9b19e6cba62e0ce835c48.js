6744fb02bb75bf9b5fddd479e267da9c96c68383
elmaranchuelo <elmaranchuelo@gmail.com>
2023-06-21 00:56:44 +0800

    Updates

---
+++ precache-manifest.9549bf2063b9b19e6cba62e0ce835c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1de39b126a1513d85611",
+    "url": "/static/css/main.7e4baf9a.chunk.css"
+  },
+  {
+    "revision": "1de39b126a1513d85611",
+    "url": "/static/js/main.1de39b12.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8266ceaf96c9ceb573c6e71d400ec180",
+    "url": "/static/media/ElmarJohnAnchueloV2.8266ceaf.pdf"
+  },
+  {
+    "revision": "ae7a330cd1abc7ab4e9390579be4ccd0",
+    "url": "/index.html"
+  }
+];
